e1159ff0c321b0d3ab8a1f76c9165352b573a29c
cuberhaus <polcg10@gmail.com>
2023-05-23 11:03:27 +0200

    test updated

--- p4/PayStationTDD - Strategy + Receipts/test/junit/com/TestIntegration.java
+++ p4/PayStationTDD - Strategy + Receipts/test/junit/com/TestIntegration.java
@@ -46,6 +46,18 @@ public class TestIntegration {
     assertEquals( "Linear Rate: 2$ should give 80 min ",
                   80 , ps.readDisplay() );
   }
+
+  @Test
+  public void ReceiptNY()
+          throws IllegalCoinException {
+    // Configure pay station to be the progressive rate pay station
+    ps = new PayStationImpl( new NYFactory() );
+    // add $ 2.0:
+    addOneDollar(); addOneDollar();
+
+    assertEquals( "Linear Rate: 2$ should give 80 min ",
+            80 , ps.buy().value() );
+  }
   /**
    * Integration testing for the progressive rate configuration
    */
@@ -59,6 +71,18 @@ public class TestIntegration {
     
     assertEquals( "Progressive Rate: 2$ should give 75 min ",
                   75 , ps.readDisplay() );
+  }
+
+  @Test
+  public void ReceiptBoston()
+          throws IllegalCoinException {
+    // reconfigure ps to be the progressive rate pay station
+    ps = new PayStationImpl(new BostonFactory());
+    // add $ 2.0: 1.5 gives 1 hours, next 0.5 gives 15 min
+    addOneDollar(); addOneDollar();
+
+    assertEquals( "Progressive Rate: 2$ should give 75 min ",
+            75 , ps.buy().value() );
   }
 
   private void addOneDollar() throws IllegalCoinException {
