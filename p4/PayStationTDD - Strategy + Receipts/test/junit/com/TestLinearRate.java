0e3d0ee3864700c88eae750f8cae16cd320380ba
cuberhaus <polcg10@gmail.com>
2023-05-23 10:30:42 +0200

    This is a placeholder

---
+++ p4/PayStationTDD - Strategy + Receipts/test/junit/com/TestLinearRate.java
@@ -0,0 +1,38 @@
+package junit.com;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/** Test the linear rate strategy.
+ 
+   This source code is from the book 
+     "Flexible, Reliable Software:
+       Using Patterns and Agile Development"
+     published 2010 by CRC Press.
+   Author: 
+     Henrik B Christensen 
+     Department of Computer Science
+     Aarhus University
+   
+   Please visit http://www.baerbak.com/ for further information.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+ 
+       http://www.apache.org/licenses/LICENSE-2.0
+ 
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+public class TestLinearRate {
+  /** Test a single hour parking */
+  @Test public void shouldDisplay120MinFor300cent() {
+    RateStrategy rs = new LinearRateStrategy();
+    assertEquals( 300 / 5 * 2, rs.calculateTime(300) ); 
+  }
+} 
